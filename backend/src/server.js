90a844f472fe890ed4b089b9bbdf0e7ae33fba00
dejuks <dejeneks1@gmail.com>
2026-03-17 14:55:31 +0300

    push with error

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,13 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import path from "path";
 
-import userRoutes from "./routes/user.routes.js";
-import authRoutes from "./routes/auth.routes.js";
 import roleRoutes from "./routes/role.routes.js";
-import permissionRoutes from "./routes/permission.routes.js";
-import rolePermissionRoutes from "./routes/rolePermission.routes.js";
-import userRoleRoutes from "./routes/userRole.routes.js";
-import moduleRoutes from "./routes/module.routes.js";
 
 import repositoryRoutes from "./repository/items/routes/repositoryItem.routes.js";
 import publicRepositoryRoutes from "./repository/items/routes/publicRepository.routes.js";
@@ -49,15 +43,11 @@ import publicManuscription  from "./manuscription/routes/public.manuscripts.rout
 
 
 import userRoutes from "./routes/user.routes.js";
-import authRoutes from "./routes/auth.routes.js";
-import roleRoutes from "./routes/role.routes.js";
 import permissionRoutes from "./routes/permission.routes.js";
 import rolePermissionRoutes from "./routes/rolePermission.routes.js";
 import userRoleRoutes from "./routes/userRole.routes.js";
 import moduleRoutes from "./routes/module.routes.js";
 import libraryRoutes from "./library/routes/library.routes.js";
-import systemSettingRoutes from "./routes/systemSetting.routes.js";
-import publisherExternalRoutes from "./routes/publisher.routes.js";
 import ebookRoutes from "./ebook/routes/ebook.routes.js";
 import ebookPublicRoutes from "./ebook/routes/ebookPublic.routes.js";
 
@@ -96,7 +86,6 @@ app.set("trust proxy", 1);
    API ROUTES
 ======================= */
 app.use("/api/auth", authRoutes);
-app.use("/api/users", userRoutes);
 app.use("/api/roles", roleRoutes);
 app.use("/api/permissions", permissionRoutes);
 app.use("/api/role-permissions", rolePermissionRoutes);
@@ -142,14 +131,6 @@ app.use("/api/journal", journalUserRoutes);
 app.use("/api/eic", eicDecisionRoutes);
 app.use("/api/payments", paymentRoutes);
 
-
-app.use("/api/auth", authRoutes);
-app.use("/api/users", userRoutes);
-app.use("/api/roles", roleRoutes);
-app.use("/api/permissions", permissionRoutes);
-app.use("/api/role-permissions", rolePermissionRoutes);
-app.use("/api/user-roles", userRoleRoutes);
-app.use("/api/modules", moduleRoutes);
 app.use("/api/system/settings", systemSettingRoutes);
 app.use("/api/publisher", publisherExternalRoutes);
 app.use("/api/ebook", ebookRoutes);
@@ -157,6 +138,7 @@ app.use("/api/ebook-public", ebookPublicRoutes);
 
 /* LIBRARY */
 app.use("/api/library", libraryRoutes);
+
 
 /* =======================
    DEFAULT ROUTE
